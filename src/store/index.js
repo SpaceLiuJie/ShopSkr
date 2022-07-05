4f2d1a022db64c507efeb3a7a5734c643cce7835
caijinpeng <cai18736948442@163.com>
2022-07-05 10:43:47 +0800

    fix:基础框架

--- src/store/index.js
+++ src/store/index.js
@@ -1,17 +1,21 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import getters from './getters'
+import persistedState from "vuex-persistedstate"
 
+const context = require.context('./modules', true, /\.js$/);
+let modules = context.keys().reduce((prev, filename, index) => {
+  let key = filename.replace(/(\.js|\.\/)/g, "");
+  prev[key] = context(filename).default;
+  return prev
+}, {})
 Vue.use(Vuex)
-
-export default new Vuex.Store({
-  state: {
-  },
-  getters: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+const store = new Vuex.Store({
+  modules,
+  getters,
+  plugins: [persistedState({
+    paths: ['user']
+  })]
 })
+
+export default store
